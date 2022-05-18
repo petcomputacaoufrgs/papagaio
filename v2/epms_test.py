311a16f24418ed95266abaf1a335e1287931200a
Vitor <crfvitor8d@hotmail.com>
2022-05-18 15:01:02 -0300

    Bringing readme back to main root

--- v2/epms_test.py
+++
@@ -1,8 +0,0 @@
-import EPMS
-import pandas as pd
-
-californication = EPMS.serialization.file("samples/Aguas De Marco.mid")
-print(californication.head().to_markdown())
-
-californication = EPMS.deserialization.file(californication)
-californication.show()
